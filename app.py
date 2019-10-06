cbfc4c48edebac05b534aa636c80bd84c7bf8792
rj0x001 <artem.e.pavlenko@gmail.com>
2019-10-06 22:40:41 +0300

    crawler

--- app.py
+++ app.py
@@ -5,32 +5,57 @@ from bs4 import BeautifulSoup
 import re
 
 
-SELECTED_URL = 'https://ru.wikipedia.org/wiki/DWDM'
+
 IMAGES = ('.jpg', '.png', '.gif')
 D = 0
-seen_sites = []
-task = []
+task = ['https://ru.wikipedia.org/wiki/DWDM', ]
+
+queue = asyncio.Queue()
+queue.put_nowait('https://ru.wikipedia.org/wiki/DWDM')
 
 
-async def get_site_content(url, dept):
+async def get_site_content(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
+            global D
+            D += 1
             text = await resp.read()
         soup = BeautifulSoup(text, features="html5lib")
-        urls = []
         content_divs = soup.findAll('div', attrs={'id': 'mw-content-text'})
         for div in content_divs:
             all_links = div.findAll('a', attrs={'href': re.compile("^/wiki/")})
             for link in all_links:
                 t = str(link.get('href'))
-                if not t.endswith(IMAGES):
+                wiki_url = 'https://ru.wikipedia.org' + t
+                if not t.endswith(IMAGES) and wiki_url not in task:
+                    task.append(wiki_url)
+                    # queue.put_nowait(wiki_url)
+                    # print(wiki_url, D, '\n')
+        print(len(task))
 
-                    print(t, '\n')
-    return urls
 
+async def foo(loop):
+    global D
+    while D < 7:
+        while task:
+            await get_site_content(task.pop())
+            # print(task)
+        # await asyncio.sleep(5)
+        return (await foo(loop))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(foo(loop))
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+"""
 loop = asyncio.get_event_loop()
 while D < 7:
     sites_soup = loop.run_until_complete(get_site_content(SELECTED_URL, D))
     print(sites_soup)
     print('stop')
 loop.close()
+"""
